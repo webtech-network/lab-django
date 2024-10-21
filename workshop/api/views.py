655cd4210a4d487323e71a667941db07b9ba0de9
Arthur <arodrigues@sga.pucminas.br>
2024-10-21 14:54:31 -0300

    Adicao de metodo getall

--- workshop/api/views.py
+++ workshop/api/views.py
@@ -1,9 +1,18 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from api.models import Task
+from api.serializers import TaskSerializer
+
+
 class BaseView(APIView):
     def get(self, request):
-        return Response("Hello World")
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+    def post(self,request):
+        pass
     
     
 
